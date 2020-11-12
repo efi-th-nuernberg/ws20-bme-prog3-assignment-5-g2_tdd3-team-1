e8a63a843b3a9cd33f12aa70ab035ca2baca82a8
repl.it user <replituser@example.com>
2020-11-12 20:01:02 +0000

    Assignment5

--- Life.java
+++ Life.java
@@ -1,14 +1,18 @@
-public class Life implements ILife {
-  
-  public static void main(String[] args) {
-    Life l = new Life(new String[] {  "     ",
-                                      "     ",
-                                      " *** ",
-                                      "     ",
-                                      "     " });
-    l = (Life) l.nextGeneration();
-  }
+import java.util.Arrays;
 
+public class Life implements ILife {
+
+
+  int M = 5;
+  int N = 5;
+  private boolean[][] present = new boolean[M][N];
+
+  public static void main(String[] args) {
+    Life l = new Life(new String[] { "     ", "     ", " *** ", "     ", "     " });
+
+    l = (Life) l.nextGeneration();
+
+  }
 
   public Life() {
     nukeAll();
@@ -22,34 +26,99 @@ public class Life implements ILife {
           setAlive(x, y);
   }
 
-
   @Override
   public void nukeAll() {
     // TODO Auto-generated method stub
-
+    present =new boolean[M][N];
   }
 
   @Override
   public void setAlive(int x, int y) {
     // TODO Auto-generated method stub
-
+    present[x][y] = true;
   }
 
   @Override
   public void setDead(int x, int y) {
     // TODO Auto-generated method stub
-
+    present[x][y] = false;
   }
 
   @Override
   public boolean isAlive(int x, int y) {
     // TODO Auto-generated method stub
-    return false;
+
+    return present[x][y];
   }
 
   @Override
   public ILife nextGeneration() {
+
+    int aliveNeighbours = 0;
+    
+   boolean[][] future = new boolean[M][N];
     // TODO Auto-generated method stub
-    return null;
+    
+    for (int x=0; x<M; x++){
+      for (int y=0; y<N; y++){
+        if (x-1 > 0 && y-1 > 0){
+          if( present[x-1][y-1] ){
+          aliveNeighbours++;
+          }
+        }
+        if (y-1 > 0){
+          if( present[x][y-1] ){
+          aliveNeighbours++;
+          }
+        }
+        if (x+1 < M && y-1 > 0){
+          if( present[x+1][y-1] ){
+          aliveNeighbours++;
+          }
+        }
+        if (x+1 < M ){
+          if( present[x+1][y] ){
+          aliveNeighbours++;
+          }
+        }
+        if (x+1 < M && y+1 < N){
+          if( present[x+1][y+1] ){
+          aliveNeighbours++;
+          }
+        }
+        if (y+1 < N){
+          if( present[x][y+1] ){
+          aliveNeighbours++;
+          }
+        }
+        if (x-1 > 0 && y+1 < N){
+          if( present[x-1][y+1] ){
+          aliveNeighbours++;
+          }
+        }
+        if (x-1 > 0 ){
+          if( present[x-1][y] ){
+          aliveNeighbours++;
+          }
+        }
+        
+
+    if ((present[x][y]) && (aliveNeighbours > 3)) {
+      future[x][y] = false;
+
+    //Rule 1
+    } else if ((!present[x][y]) && (aliveNeighbours >= 3)) {
+      future[x][y] = true;
+    // Rule 2
+    } else if ((present[x][y]) && (aliveNeighbours < 2)) {
+      future[x][y] = false;
+    //Rule 3
+    } else if ((present[x][y]) && (aliveNeighbours == 2 || aliveNeighbours == 3)) {
+      future[x][y] = true;
+    }
+    }
+  }
+    present = future;
+    return this;
   }
 }
